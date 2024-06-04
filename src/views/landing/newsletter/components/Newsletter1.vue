de03983a2d8c0e542257b9daaa9a30da70e15f8f
y_takino <tapppe9@gmail.com>
2024-06-04 13:50:37 +0900

    Update 取得した情報を表示

--- src/views/landing/newsletter/components/Newsletter1.vue
+++ src/views/landing/newsletter/components/Newsletter1.vue
@@ -1,15 +1,43 @@
-<!--
-* @Component:
-* @Maintainer: J.K. Yang
-* @Description:
--->
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import axios from "axios";
+
+const amazonUrl = ref("");
+const manufacturer = ref("");
+const deviceName = ref("");
+const price = ref("");
+const thumbnailUrl = ref("");
+const loading = ref(false);
+
+const fetchData = async () => {
+  loading.value = true;
+  if (!amazonUrl.value) {
+    console.error("URL is empty");
+    loading.value = false;
+    return;
+  }
+
+  try {
+    const response = await axios.post(
+      "http://localhost:5000/api/scrape-amazon",
+      { url: amazonUrl.value }
+    );
+    const data = response.data;
+    manufacturer.value = data.manufacturer;
+    deviceName.value = data.deviceName;
+    price.value = data.price;
+    thumbnailUrl.value = data.thumbnailUrl;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+  loading.value = false;
+};
+</script>
 
 <template>
   <v-toolbar height="60">
     <v-toolbar-title class="text-h6 font-weight-bold">
-      <!-- <v-icon class="mr-2">mdi-account</v-icon> -->
-      <span>Newsletter1</span>
+      <span>Add Amazon Device</span>
     </v-toolbar-title>
   </v-toolbar>
 
@@ -28,23 +56,70 @@
       <v-container>
         <v-row align="center" justify="center">
           <v-col cols="12" lg="3">
-            <h1 class="text-h5 font-weight-bold text-center">
-              Receive updates in your inbox
-            </h1>
+            <h1 class="text-h5 font-weight-bold text-left">Input URL</h1>
+            <p>メーカ、デバイス名、価格、サムネイル画像URLを取得する</p>
           </v-col>
           <v-col cols="12" sm="9" md="7">
             <v-text-field
-              class=""
               color="primary"
               hide-details
               variant="outlined"
-              placeholder="email"
+              placeholder="urlを入力してください"
+              v-model="amazonUrl"
+              @keyup.enter="fetchData"
             ></v-text-field>
           </v-col>
           <v-col cols="12" sm="3" md="2">
-            <v-btn size="x-large" block color="primary font-weight-bold"
+            <v-btn
+              size="x-large"
+              block
+              color="primary font-weight-bold"
+              @click="fetchData"
+              :loading="loading"
               >Subscribe</v-btn
             >
+          </v-col>
+        </v-row>
+        <v-row v-if="manufacturer || deviceName || price || thumbnailUrl">
+          <v-col cols="12" sm="6">
+            <v-text-field
+              label="メーカー"
+              v-model="manufacturer"
+              variant="outlined"
+              clearable
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" sm="6">
+            <v-text-field
+              label="デバイス名"
+              v-model="deviceName"
+              variant="outlined"
+              clearable
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" sm="6">
+            <v-text-field
+              label="価格"
+              v-model="price"
+              variant="outlined"
+              clearable
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" sm="6">
+            <v-text-field
+              label="サムネイルURL"
+              v-model="thumbnailUrl"
+              variant="outlined"
+              clearable
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12">
+            <v-img
+              v-if="thumbnailUrl"
+              :src="thumbnailUrl"
+              alt="Thumbnail"
+              max-width="200"
+            ></v-img>
           </v-col>
         </v-row>
       </v-container>
